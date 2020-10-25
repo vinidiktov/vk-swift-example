f41d519268163730791a0503b56575919044426e
Alexei Vinidiktov <alexei@vinidiktov.ru>
2020-10-25 16:43:22 +0700

    Initial Commit

---
+++ VK_Swift_Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK_Swift_Example
+//
+//  Created by Alex Vinidiktov on 25.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
